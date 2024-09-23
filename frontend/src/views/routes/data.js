0185641f108fa36961b0b405c379b7cb7c0928b0
Utopiality7 <akirameruna7315@gmail.com>
2024-09-23 05:24:56 -0400

    feat: implement uuid in route data file

--- frontend/src/views/routes/data.js
+++ frontend/src/views/routes/data.js
@@ -1,3 +1,5 @@
+import uuid from "uuid";
+
 import StreamList from "../pages/streams/list";
 import StreamCreate from "../pages/streams/create";
 import StreamEdit from "../pages/streams/edit";
@@ -5,22 +7,27 @@ import StreamDelete from "../pages/streams/delete";
 import StreamShow from "../pages/streams/show";
 const routesList = [
   {
+    id: uuid.v4(),
     path: "/",
     element: <StreamList />,
   },
   {
+    id: uuid.v4(),
     path: "/streams/new",
     element: <StreamCreate />,
   },
   {
+    id: uuid.v4(),
     path: "/streams/edit",
     element: <StreamEdit />,
   },
   {
+    id: uuid.v4(),
     path: "/streams/delete",
     element: <StreamDelete />,
   },
   {
+    id: uuid.v4(),
     path: "/streams/show",
     element: <StreamShow />,
   },
